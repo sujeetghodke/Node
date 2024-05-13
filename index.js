0834f9a8bc09cab08b89cb180279b443dd98a4a5
sujeetghodke <sujeetghodke16@gmail.com>
2024-05-13 15:02:51 +0530

    .

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const http = require("http");
+const fs = require("fs");
+const url = require("url");
 
 const myServer = http.createServer((req, res) => {
   //   console.log("New Request Received..");
-  console.log(req.headers);
-  res.end("hello from server ");
+  const log = `${Date.now()} : ${req.url} new Request received\n`;
+  const myUrl = url.parse(req.url, true);
+  console.log(myUrl);
+  fs.appendFile("log.txt", log, (err, data) => {
+    switch (myUrl.pathname) {
+      case "/":
+        res.end("hello from server ");
+        break;
+      case "/about":
+        res.end("Hello my name is Sujeet Ghodke");
+        break;
+      default:
+        res.end("404 page not found");
+    }
+  });
 });
 
 myServer.listen(8000, () => {
