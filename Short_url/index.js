cc4b5c3e96051f6ccd09bd0324804135da209522
sujeetghodke <sujeetghodke16@gmail.com>
2024-05-21 21:12:42 +0530

    code updated

--- Short_url/index.js
+++ Short_url/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { connectToMongoDb } = require("./connect");
+const URL = require("./models/url");
 const urlRoute = require("./routes/url");
 
 const app = express();
@@ -9,7 +10,26 @@ connectToMongoDb("mongodb://localhost:27017/short-url").then(
   console.log("Mongodb connected :)")
 );
 
+app.use(express.json());
+
 app.use("/url", urlRoute);
+
+app.get("/:shortId", async (req, res) => {
+  const shortId = req.params.shortId;
+  const entry = await URL.findOneAndUpdate(
+    {
+      shortId,
+    },
+    {
+      $push: {
+        visitHistory: {
+          timestamp: Date.now(),
+        },
+      },
+    }
+  );
+  res.redirect(entry.redirectUrl);
+});
 
 app.listen(PORT, () => {
   console.log(`Server is Live on Port : ${PORT}`);
